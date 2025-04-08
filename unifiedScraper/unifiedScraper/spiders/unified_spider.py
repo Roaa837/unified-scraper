231c33f6fe20aba0171c3b5fd509e120e09f6e0e
Abdalla Darwesh <a.darwesh@tay.local>
2025-04-08 16:33:17 +0200

    adding exception hanler to deal with missleading urls

--- unifiedScraper/unifiedScraper/spiders/unified_spider.py
+++ unifiedScraper/unifiedScraper/spiders/unified_spider.py
@@ -1,6 +1,7 @@
 import json
 import scrapy
 from pathlib import Path
+from urllib.parse import urljoin
 from ..items import UnifiedscraperItem
 
 
@@ -12,6 +13,13 @@ class UnifiedSpider(scrapy.Spider):
         self.website = website
         self.config = self.load_config()
         self.selectors = self.config['selectors']
+
+        try:
+            self.custom_settings = self.config['custom_settings']
+
+        except KeyError:
+            pass
+
 
     def load_config(self):
         config_path = Path(__file__).parent.parent / 'configs' / 'websites.json'
@@ -33,11 +41,16 @@ class UnifiedSpider(scrapy.Spider):
         brands_names = response.css(self.selectors['site-brands-names']).getall()
 
         for brand_url , brand_name in zip(brands_urls,brands_names):
-            yield response.follow(
-                brand_url,
-                callback = self.parse_brand_page,
-                meta = {'brand_name' : brand_name}
-            )
+            absolute_url = self.make_absolute_url(response, brand_url)
+            try:
+                yield response.follow(
+                    absolute_url,
+                    callback = self.parse_brand_page,
+                    meta = {'brand_name' : brand_name}
+                )
+            except:
+                yield self.parse_brand_page(response)
+
 
     def parse_brand_page(self , response):
         brand_item = UnifiedscraperItem()
@@ -47,3 +60,9 @@ class UnifiedSpider(scrapy.Spider):
         brand_item['number_of_products'] = '0' if brand_products is None else brand_products
 
         return brand_item
+
+    def make_absolute_url(self, response, url):
+        """Convert any URL to absolute form"""
+        if url.startswith(('http://', 'https://')):
+            return url  # Already absolute
+        return urljoin(response.url, url)  # Handle relative paths
