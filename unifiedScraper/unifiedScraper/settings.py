354f61094554caec700df4e80af4769c07a0ea0f
Abdalla Darwesh <a.darwesh@tay.local>
2025-04-08 11:53:35 +0200

    modify the crawler to diobey the robottxt file

--- unifiedScraper/unifiedScraper/settings.py
+++ unifiedScraper/unifiedScraper/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "unifiedScraper.spiders"
 #USER_AGENT = "unifiedScraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
