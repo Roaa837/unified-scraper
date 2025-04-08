93b15de1636dddf13ea400546d0f57e71ce416fa
Abdalla Darwesh <a.darwesh@tay.local>
2025-04-08 13:33:48 +0200

    declaring the returned item object

--- unifiedScraper/unifiedScraper/items.py
+++ unifiedScraper/unifiedScraper/items.py
@@ -9,4 +9,5 @@ import scrapy
 class UnifiedscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    brand_name = scrapy.Field()
+    number_of_products = scrapy.Field()
